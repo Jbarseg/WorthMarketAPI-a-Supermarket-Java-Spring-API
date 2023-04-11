665e6ca537cd10ff20c1f92987fbe36b933ac5c4
jbarseg <jhonnyd08@gmail.com>
2023-04-10 23:52:15 -0600

    Update CategoriaRepository.java

--- src/main/java/com/github/jbarseg/persistence/CategoriaRepository.java
+++ src/main/java/com/github/jbarseg/persistence/CategoriaRepository.java
@@ -3,7 +3,6 @@ package com.github.jbarseg.persistence;
 import java.util.List;
 import java.util.Optional;
 
-import org.checkerframework.checker.nullness.Opt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
